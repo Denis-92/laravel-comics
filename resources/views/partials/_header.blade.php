1dc80641861a12869739e1683be07fa4fb242064
Denis-92 <d.mozenic@gmail.com>
2022-11-06 20:01:47 +0100

    add header content menu

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -6,7 +6,18 @@
         <div>
             <img src="{{ asset('images/dc-logo.png') }}" alt="Image">
         </div>
-        <div>CHARACTERS</div>
-        <div>COMICS</div>
+        <div class="flex flex-between-x border margin-horiz-1">
+            <div>CHARACTERS</div>
+            <div>COMICS</div>
+            <div>MOVIES</div>
+            <div>TV</div>
+            <div>GAMES</div>
+            <div>COLLECTIBLES</div>
+            <div>VIDEOS</div>
+            <div>FANS</div>
+            <div>NEWS</div>
+            <div>SHOP</div>
+        </div>
+        <div>Search</div>
     </div>
 </header>
