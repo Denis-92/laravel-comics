83c60daf7e36c14aaa47b868d326d5e4c167c7a5
Denis-92 <d.mozenic@gmail.com>
2022-11-06 17:16:52 +0100

    add style to header

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -2,7 +2,7 @@
     <div id="header-top-bar">
 
     </div>
-    <ul class="flex container">
+    <ul class="flex container flex-center-y flex-between-x">
         <img src="{{ asset('images/dc-logo.png') }}" alt="Image">
         <li>CHARACTERS</li>
         <li>COMICS</li>
